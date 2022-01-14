c504c1363cfc48586fc51a3db4dbe0b816194962
Monxun <michaelgibney1@gmail.com>
2022-01-14 00:07:21 -0700

    updated

--- week 1/4/4b.py
+++ week 1/4/4b.py
@@ -6,5 +6,5 @@ orders = [
     [88112, "Einfuhrung in Python3, Bernd Klein", 3, 24.99]
 ]
 
-updated_orders = map(lambda x: (x[0], x[2]*x[3]) if (x[2]*x[3]) >= 100 else (x[0], x[2]*x[3]+10), orders)
-print(list(updated_orders))
+updated_orders = list(map(lambda x: (x[0], x[2]*x[3]) if (x[2]*x[3]) >= 100 else (x[0], x[2]*x[3]+10), orders))
+print(updated_orders)
